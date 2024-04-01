cf9ca73b2045fb47d7fc9a1d0c8c0b597f6df864
Davi Toreti <122990430+Davitoreti@users.noreply.github.com>
2024-04-01 17:43:55 -0300

    desenvolvendo os scripts do projeto

---
+++ app/conectaAPI.js
@@ -0,0 +1,29 @@
+async function listaVideos () {
+    const conexao = await fetch('http://localhost:3000/videos');
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida;
+}
+
+async function criarVideo () {
+    const conexao = await fetch('http://localhost:3000/videos', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+export const conectaAPI = {
+    listaVideos, 
+    criarVideo
+}
